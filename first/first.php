3947cfa33c0cefb8f0c060e4ca10aa48b958705f
Amoralchik <dariysdemonad@gmail.com>
2025-01-06 12:46:43 +0200

    Add demonstration of string manipulation and formatting in PHP

--- first/first.php
+++ first/first.php
@@ -32,3 +32,54 @@ var_dump($scores);
 $total = $scores[0] + $scores[1] + $scores[2];
 echo "Total: $total\n";
 echo "Total score is" . $total . "\n";
+
+# some way to write string
+$singleQuote = 'Single quote cannot use $isStudent' . "\n";
+$doubleQuote = "Double quote can use $isStudent\n";
+
+$heredoc = <<<EOT
+Heredoc can use $isStudent
+EOT;
+
+$nowdoc = <<<'EOT'
+Nowdoc cannot use $isStudent
+EOT;
+
+echo $singleQuote;
+echo $doubleQuote;
+echo $heredoc;
+echo $nowdoc;
+
+# string manipulation
+$str = "Hello World!";
+echo $str[0]; # H
+echo $str[-1] . "\n"; # !
+// H!
+
+echo substr($str, 0, 5); # Hello
+echo substr($str, 6); # World!
+// Hello World!
+
+echo strtoupper($str) . "\n"; # HELLO WORLD!
+echo ucfirst(strtolower($str)); # Hello world!
+
+$greeting = "Hello";
+$greeting .= " World!";
+echo $greeting . "\n"; # Hello World!
+
+$haystack = "The quick brown fox";
+$pos = strpos($haystack, "quick"); # 4
+
+var_dump($pos, str_replace("quick", "slow", $haystack)); # 4, The slow brown fox
+
+printf("%s eat %d pizzas", $name, $pizzas); # Amo eat 5 pizzas
+
+$csv = 'apple,banana,cherry';
+$fruits = explode(',', $csv);
+var_dump($fruits); # ["apple", "banana", "cherry"]
+var_dump(implode(' ', $fruits)); # apple banana cherry
+
+$padded = str_pad("Hello", 10, "-", STR_PAD_BOTH);
+echo $padded . "\n"; # --Hello---
+var_dump(trim($padded, "-")); # Hello
+var_dump(trim("      Hello    ")); # Hello

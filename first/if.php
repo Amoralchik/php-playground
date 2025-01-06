d604aa2f9d77435ddbc6474e02c27d60b96a1b21
Amoralchik <dariysdemonad@gmail.com>
2025-01-06 07:40:59 +0200

    Add match expression demonstration for HTTP status handling

--- first/if.php
+++ first/if.php
@@ -57,3 +57,15 @@ switch ($size) {
   default:
     echo "Unknown Size\n";
 }
+
+# match expression
+
+$status = 404;
+
+$message = match ($status) {
+  200,300 => "OK",
+  400, 404,500 => "Error",
+  default => "Unknown Status",
+};
+
+echo $message . "\n";

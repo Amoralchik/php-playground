31f06d5f9f0f6827d0e1cf45321bd55a56ec0f94
Amoralchik <dariysdemonad@gmail.com>
2025-01-10 08:27:39 +0200

    Add greeting route and fallback redirection in web routes

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greet/{name}', function ($name) {
+    return "Hello $name!";
+});
+
+Route::fallback(function () {
+    return redirect()->route('root');
+});
+
+// GET
+// POST
+// PUT
+// DELETE

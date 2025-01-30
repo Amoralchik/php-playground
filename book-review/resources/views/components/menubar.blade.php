d6bc2e0cc727037226f3eb4adb74dcdf705dee26
Amoralchik <dariysdemonad@gmail.com>
2025-01-30 22:21:03 +0200

    Add menubar component and integrate it into the app layout

---
+++ book-review/resources/views/components/menubar.blade.php
@@ -0,0 +1,12 @@
+<nav class="bg-gray-300 text-black">
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex items-center justify-between h-16">
+    <div class="flex-shrink-0">
+        <h2 class="font-bold text-2xl tracking-tight"> Bookstore </h2>
+    </div>
+    <div class="hidden md:block space-x-10">
+        <a href={{ route('books.index') }} class="text-gray-900 hover:gray-700">Home</a>
+        <a href={{ route('books.create') }} class="text-gray-900 hover:gray-700">Add Book</a>
+    </div>
+</div>
+</nav>
--- book-review/resources/views/layout/app.blade.php
+++ book-review/resources/views/layout/app.blade.php
@@ -8,12 +8,12 @@
     <title>@yield('title')</title>
 </head>
 
-<body class="bg-gray-200 p-4 min-h-screen text-gray-800 font-normal leading-relaxed tracking-wide">
-    <div class="container mx-auto max-w-3xl px-4">
-        <h1 class="text-8xl font-black text-center uppercase">@yield('title')</h1>
-    </div>
-    <div class="flex flex-col items-center justify-center gap-8 mt-8 text-gray-600">
-        @yield('content')
+<body>
+    @include('components.menubar')
+    <div class="bg-gray-200 p-4 min-h-screen text-gray-800 font-normal leading-relaxed tracking-wide">
+        <div class="flex flex-col items-center justify-center gap-8 mt-8 text-gray-600">
+            @yield('content')
+        </div>
     </div>
 </body>
 
